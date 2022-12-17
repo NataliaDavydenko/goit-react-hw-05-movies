9fa76648d2d8ac830ed8f93424faf894ad2bdd1f
Natalia <davydenko.n@ukr.net>
2022-12-18 00:42:31 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,23 @@
-import { Routes, Route } from "react-router-dom";
-import { Layout } from "./Layout/Layout";
-import { Home } from "pages/Home/Home";
-import { Movies } from "pages/Movies/Movies";
+import { Routes, Route } from 'react-router-dom';
+import { Layout } from './Layout/Layout';
+import { Home } from 'pages/Home/Home';
+import { Movies } from 'pages/Movies/Movies';
+import { MovieDetails } from 'pages/MovieDetails/MovieDetails';
+import { Cast } from './Cast/Cast';
+import { Reviews } from './Reviews/Reviews';
 
 export const App = () => {
   return (
     <div>
       <Routes>
-        <Route path='/' element={<Layout />}>
+        <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-            <Route path="/movies" element={<Movies />} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="movies/:movieId" element={<MovieDetails />}>
+            <Route path="cast" element={<Cast />} />
+            <Route path="reviews" element={<Reviews />} />
+          </Route>
+          <Route path="*" element={<Home />} />
         </Route>
       </Routes>
     </div>
